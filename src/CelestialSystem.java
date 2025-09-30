53ffa4d674cb398d1cb793fc985fd76f9b6c00cb
Andres <andrescalvo01@gmail.com>
2025-09-30 13:12:54 +0200

    Ajuste de Main y filtración

--- src/CelestialSystem.java
+++ src/CelestialSystem.java
@@ -1,7 +1,8 @@
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Iterator;
 
-public class CelestialSystem {
+public class CelestialSystem implements Iterable<CelestialBodies> {
     private List<CelestialBodies> bodies = new ArrayList<>();
 
     public void addBody(CelestialBodies body) {
@@ -11,4 +12,35 @@ public class CelestialSystem {
     public List<CelestialBodies> getBodies() {
         return bodies;
     }
+
+    public List<CelestialBodies> listAll() {
+        return bodies;
+    }
+
+    public List<Planet> planetsWithAtmosphere() {
+        List<Planet> result = new ArrayList<>();
+        for (CelestialBodies b : bodies) {
+            if (b instanceof Planet p && p.hasAtmosphere()) {
+                result.add(p);
+            }
+        }
+        return result;
+    }
+
+    public List<Moon> moonsOf(String planetName) {
+        List<Moon> result = new ArrayList<>();
+        for (CelestialBodies b : bodies) {
+            if (b instanceof Moon m && m.getOrbitingPlanet().equalsIgnoreCase(planetName)) {
+                result.add(m);
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public Iterator<CelestialBodies> iterator() {
+        return bodies.iterator();
+    }
 }
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,36 @@
 public class Main {
     public static void main(String[] args) {
+
         CelestialSystem system = new CelestialSystem();
         system.addBody(new Planet("Earth", 12742, 24, true, 1));
         system.addBody(new Star("Sun", 1397000, 23, 5778, "G2V"));
         system.addBody(new Moon("Moon", 3474, 22, "Earth", true));
 
-        CelestialQueries queries = new CelestialQueries();
         HabitabilityPrinter printer = new HabitabilityPrinter();
 
-        System.out.println(" Descriptions: ");
-        System.out.println(" ");
-        for (CelestialBodies body : system.getBodies()) {
-            System.out.println(body.getDescription());
+
+        System.out.println("All Celestial Bodies: ");
+        for (CelestialBodies b : system.listAll()) {
+            System.out.println(b.getDescription());
         }
 
-        System.out.println(" ");
-        System.out.println("Habitability Report: ");
-        System.out.println(" ");
-        for (CelestialBodies body : system.getBodies()) {
-            if (body instanceof Habitable habitable) {
-                printer.printReport(habitable);
-                System.out.println();
+
+        System.out.println("\n Planets with Atmosphere: ");
+        for (Planet p : system.planetsWithAtmosphere()) {
+            System.out.println(p.getDescription());
+        }
+
+
+        System.out.println("\n Moons that orbit Earth: ");
+        for (Moon m : system.moonsOf("Earth")) {
+            System.out.println(m.getDescription());
+        }
+
+
+        System.out.println("\n Habitability Report: ");
+        for (CelestialBodies b : system) {
+            if (b instanceof Habitable hab) {
+                printer.printReport(hab);
             }
         }
     }
